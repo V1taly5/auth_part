ebf1b1cd6cffadf63edad65db56f76437b49807e
V1taly5 <egorov21112061vito@gmail.com>
2025-04-22 18:22:21 +0300

    feat: added the ability to check if the method was called

--- internal/lib/mail/mail.go
+++ internal/lib/mail/mail.go
@@ -6,7 +6,9 @@ type Sender interface {
 	SendIPChangeWarning(email, oldIP, newIP string) error
 }
 
-type MockEmailSender struct{}
+type MockEmailSender struct {
+	Called bool
+}
 
 func NewMockEmailSender() Sender {
 	return &MockEmailSender{}
@@ -15,5 +17,6 @@ func NewMockEmailSender() Sender {
 func (s *MockEmailSender) SendIPChangeWarning(email, oldIP, newIP string) error {
 	log.Printf("[MOCK EMAIL] To: %s, Subject: Security Alert - IP Address Change\n", email)
 	log.Printf("[MOCK EMAIL] Body: We detected a new login from IP: %s (previous: %s)\n", newIP, oldIP)
+	s.Called = true
 	return nil
 }
